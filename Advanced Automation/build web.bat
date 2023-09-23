9c4097a1ceecbbfc0fdc98d46e7a34b2f833f861
Werxzy <Werxzyz@yahoo.com>
2023-09-22 20:24:30 -0500

    Added arguments for filename, margin, and embed color. Added commented versions that contain the resulting code. Added .gitignore file for ignoring the built files.

--- Advanced Automation/build web.bat
+++ Advanced Automation/build web.bat
@@ -1,16 +1,17 @@
-set "name=1k_car"
+set name=1k_car
+set pico8="C:\Program Files (x86)\PICO-8\pico8.exe"
 
 @REM shrink and export minified version
 @REM X:\shrinko8\shrinko8 %name%.p8 %name%_mini.p8 --minify-safe-only --script add_comment.py
 
 @REM export to html
-@REM "C:\Program Files (x86)\PICO-8\pico8.exe" %name%_mini.p8 -export "-f %name%.html"
-"C:\Program Files (x86)\PICO-8\pico8.exe" ..\%name%.p8 -export "-f %name%.html"
+@REM %pico8% %name%_mini.p8 -export "-f %name%.html"
+%pico8% ..\%name%.p8 -export "-f %name%.html"
 
 @REM fix formatting and colors
 python process_web_version.py %name%
 
 @REM zip it
-"C:\Program Files (x86)\WinRAR\WinRAR.exe" a -afzip ".\%name%_html.zip" %name%_html
+"C:\Program Files (x86)\WinRAR\WinRAR.exe" a -afzip "%name%_html.zip" %name%_html
 
 timeout 3
--- Simple Automation/build versions.bat
+++ Simple Automation/build versions.bat
@@ -1,20 +1,27 @@
-@REM replace name of game and file location of pico8
+@REM replace name of game and file locations where necessary
 set name=1k_car
 set pico8="C:\Program Files (x86)\PICO-8\pico8.exe"
+set margin=0
+set color=22222200
 
 @REM export to html
 %pico8% ..\%name%.p8 -export "-f %name%.html"
+@REM "C:\Program Files (x86)\PICO-8\pico8.exe" ..\1k_car.p8 -export "-f 1k_car.html"
 
-@REM fix formatting
-python process_web_version.py
+@REM fix content of web version
+python process_web_version.py %name% %margin% %color%
+@REM python process_web_version.py 1k_car 0 22222200
 
-@REM zip it
-"C:\Program Files (x86)\WinRAR\WinRAR.exe" a -afzip "%name% html.zip" %name%_html\index.html %name%_html\%name%.js
+@REM zip web files for itch.io page
+"C:\Program Files (x86)\WinRAR\WinRAR.exe" a -afzip "%name%_html.zip" %name%_html\index.html %name%_html\%name%.js
+@REM "C:\Program Files (x86)\WinRAR\WinRAR.exe" a -afzip "1k_car_html.zip" 1k_car_html\index.html 1k_car_html\1k_car.js
 
 @REM export cart
 %pico8% ..\%name%.p8 -export "%name%.p8.png"
+@REM "C:\Program Files (x86)\PICO-8\pico8.exe" ..\1k_car.p8 -export "1k_car.p8.png"
 
 @REM export binary versions and the cart
 %pico8% ..\%name%.p8 -export "%name%.bin"
+@REM "C:\Program Files (x86)\PICO-8\pico8.exe" ..\1k_car.p8 -export "1k_car.bin"
 
 timeout 3
